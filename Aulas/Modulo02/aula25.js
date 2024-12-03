f680d1dc5190f2464b7123955c2e936b15237f8e
Railton Perenha <34631338+railtonperenha@users.noreply.github.com>
2024-12-03 18:26:22 -0300

    Update aula25.js

--- Aulas/Modulo02/aula25.js
+++ Aulas/Modulo02/aula25.js
@@ -1,7 +1,43 @@
-let fruits = ['Banana', 'Laranja', 'Maçã', 'Pêra'];
+let fruits = ['Banana', 'Laranja', 'Maçã', 'Pêra', 'Uva'];
 
-let bigFruits = fruits.filter(() => {
-    if(item.length > 4) {
-        return true;
-    }
-})
+//MÉTODO FILTER():
+// let bigFruits = fruits.filter((item) => {
+//     return item.length > 4; //FORMA SIMPLIFICADA: se item tiver mais que 4 caracteres, retorna true.
+    
+//     // if(item.length > 4) { //FORMA DETALHADA
+//     //     return true;
+//     // } else {
+//     //     return false;
+//     // }
+// });
+// ----------------------------------------------------------
+
+//MÉTODO EVERY()'TODOS':
+// let ok = fruits.every((value) => {
+//     return value.length > 3;
+// });
+
+// if (ok) {
+//     console.log('Todos são maiores que 3');
+// } else {
+//     console.log('Não são todos maiores que 3');
+// };
+// ----------------------------------------------------------
+
+//MÉTODO SOME() 'ALGUM':
+// let ok = fruits.some((value) => {
+//     return value.length > 3;
+// });
+
+// if (ok) {
+//     console.log('Algum item é maior que 3');
+// } else {
+//     console.log('Nenhum item é maior que 3');
+// };
+
+//MÉTODO INCLUDES():
+if (fruits.includes('Uva')) {
+    console.log('Tem uva sim!');
+} else {
+    console.log('Não tem uva!')
+};
